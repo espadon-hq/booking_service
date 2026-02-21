d6e94e56e717827f827c712a792ca5746d30a141
espadon-hq <vitashi02@gmail.com>
2026-02-21 16:08:04 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='lab1',
+    version='0.0.1',
+    license='MIT',
+    author='Vitaliy Shimanskyi',
+    author_email='vitashi02@gmail.com',
+    description='lab1',
+    packages=find_packages()
+)
